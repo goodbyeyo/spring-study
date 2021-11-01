17acd454d8f8848339a52bd80684b6f6bee3f13a
sang wook <wook>
2021-11-01 11:30:32 +0900

    using object with singleton pattern applied

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -6,6 +6,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class SingletonTest {
 
     @Test
@@ -24,6 +26,21 @@ public class SingletonTest {
         System.out.println("memberService2 = " + memberService2);
 
         // memberService1 != memberService
-        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        System.out.println("singletonService1 = " + singletonService1);
+        SingletonService singletonService2 = SingletonService.getInstance();
+        System.out.println("singletonService1 = " + singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+        // isSameAs는 == 연산자와 같다
+        // equals 는 equals() 메서드와 같다
+        
+        
     }
 }
