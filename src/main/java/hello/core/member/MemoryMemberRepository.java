9c264d94670d995b321c26e2bf6601479252adf9
sang wook <wook>
2021-11-02 11:25:31 +0900

    Component scan

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,8 +1,11 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class MemoryMemberRepository implements MemberRepository{
 
     // 동시성의 문제가 있는곳에서는 ConCurrentHashMap을 쓰는것이 좋다다
