13bc16762cf77a7c3732d639df2ae8b3790494c2
sang wook <wook>
2021-11-02 13:13:56 +0900

    dependency injection with constructor

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -15,12 +15,36 @@ public class OrderServiceImpl implements OrderService {
     // private final MemberRepository memberRepository = new MemoryMemberRepository();
     // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     // private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-    // final 이 있으면 값이 반드시 할당되어야 한다(보통 생성자)
-    private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy;
+    // final 이 있으면 값이 반드시 할당되어야 한다(보통 생성자를 통해 값을 반드시 넣도록 구현)
+    // 단 생성자가 1개만 있으면 @Autowired 를 생략해도 된다
+
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
+
+    /*
+        @Autowired //(required = false) 선택적 의존관계에 사용할수있다
+        public void setMemberRepository(MemberRepository memberRepository) {
+            System.out.println("memberRepository = " + memberRepository);
+            this.memberRepository = memberRepository;
+
+        }
+
+        @Autowired
+        public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+            System.out.println("discountPolicy = " + discountPolicy);
+            this.discountPolicy = discountPolicy;
+        }
+    */
+    
+    @Autowired
+    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        System.out.println("1.OrderServiceImpl.OrderServiceImpl");
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
